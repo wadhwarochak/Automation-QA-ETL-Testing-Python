86a586aa696fc03fa14532351130a2e228d946a3
wadhwarochak <rochak.wadhwa@gmail.com>
2026-01-07 19:05:25 +0530

    Strength - 07-Jan

--- sales_table_validation.py
+++ sales_table_validation.py
@@ -21,6 +21,12 @@ def test_presence_of_records_in_tables(tables):
         select count(*) from {tables}
     ''')[0][0] >=1
 
+def test_column_names_in_tables():
+    assert run_sql_query_for_validation(f'''
+    select column_name from information_schema.columns where table_name = 'sales'
+    ''') == ['SaleID','SaleDate','CustomerName','ProductName','Quantity','UnitPrice','TotalAmount', 'CreatedAt']
+
 if __name__ == "__main__":
     test_availability_of_tables()
     test_presence_of_records_in_tables()
+    test_column_names_in_tables()
